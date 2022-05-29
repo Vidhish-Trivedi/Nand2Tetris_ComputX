249a3019abc395f8a611718983597f7176527844
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2022-05-29 19:12:14 +0530

    Added file output logic.
    The file machine.txt is an output file generated to test my own work.

--- 06/Assembler.py
+++ 06/Assembler.py
@@ -11,7 +11,7 @@ list_ins_labels = []
 # instructions in assembly file should be written without spaces,
 # That is: D=A+D is accepted, D = A + D is not accepted.
 # There should be an empty line at the end of the assembly file.
-assembly = fileinput.input("assembly.txt")
+assembly = fileinput.input("./assembly.txt")
 
 for line in assembly:
     # remove blank lines (white spaces).
@@ -22,5 +22,31 @@ for line in assembly:
         # print(line, end="")
         list_ins_labels.append(line[:len(line) - 1])
 
-# for _ in list_ins_labels:
-#     print(_)
+# count number of instructions.
+ins_count = 0
+for _ in list_ins_labels:
+    print(_)
+    if(_[0] != "("):
+        ins_count += 1
+
+label_count = len(list_ins_labels) - ins_count
+
+# print(ins_count)
+# print(label_count)
+
+########################################################
+
+
+# BINARY CONVERSION LOGIC.
+
+
+########################################################
+
+
+# # open file in write mode.
+# machineLang = open("./machine.txt", "w")
+# # write to output file.
+# for line in list_ins_labels:
+#     machineLang.write(line)
+#     machineLang.write("\n")
+# machineLang.close()
